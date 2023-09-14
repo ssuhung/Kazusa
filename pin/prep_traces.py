3596d5020536e73d5e1cb3a134b91330cef783be
Ssuhung Yeh <ssuhung@gmail.com>
2023-09-14 19:35:10 +0000

    Prepare scripts to be published

--- pin/prep_traces.py
+++ pin/prep_traces.py
@@ -4,13 +4,13 @@ import os
 import numpy as np
 import progressbar
 
-# Parameters
+
+# Parameters (Set these by yourself)
 num_worker = 8
-manifold_root = '/home/ssuhung/Downloads/Manifold-SCA/'
-target_path = manifold_root + 'target/dwebp'
-npz_output_root = manifold_root + "data/CelebA_WebP/pin/raw/"
-img_root = manifold_root + 'data/CelebA_WebP/image/'
-splits = ['train/', 'test/']
+repo_root = ''
+target_path = repo_root + 'target/tjexample'
+npz_output_root = repo_root + 'data/CelebA_jpg/pin/raw/'
+img_root = repo_root + 'data/CelebA_jpg/image/'
 
 def make_path(path):
     if not os.path.exists(path):
@@ -45,6 +45,7 @@ def raw2npz(in_path, npz_path) -> int:
 widgets = ['Progress: ', progressbar.Percentage(), ' ', 
             progressbar.Bar('#'), ' ', 'Count: ', progressbar.Counter(), ' ',
             progressbar.Timer(), ' ', progressbar.ETA()]
+splits = ['train/', 'test/']
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -77,8 +78,10 @@ if __name__ == '__main__':
             prefix = img.split('.')[0]
             npz_path = npz_output_root + split + prefix + '.npz'
             
-            # os.system('LD_LIBRARY_PATH=/opt/libjpeg-turbo/lib64 %s -- %s %s %s > /dev/null' % (pin, target_path, img_path, 'img_output_'+ str(args.ID)+'.bmp'))
-            os.system(f'{pin} -- {target_path} {img_path} -bmp -o {"img_output_" + str(args.ID) + ".bmp"} > /dev/null 2>&1')
+            # Target libjpeg
+            os.system(f'{pin} -- {target_path} {img_path} {"img_output_" + str(args.ID) + ".bmp"} > /dev/null')
+            # Target libwebp
+            # os.system(f'{pin} -- {target_path} {img_path} -bmp -o {"img_output_" + str(args.ID) + ".bmp"} > /dev/null 2>&1')
 
             leng = raw2npz(in_path=pin_out, npz_path=npz_path)
             max_len = max(max_len, leng)
